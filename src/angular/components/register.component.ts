2591e2071e54be14044507a0b9730b448c3750e2
rileyg98 <rileyg98@users.noreply.github.com>
2019-12-08 22:13:53 +1000

    Changed to add oidcfill
    Added oidcfill

--- src/angular/components/register.component.ts
+++ src/angular/components/register.component.ts
@@ -61,9 +61,10 @@ export class RegisterComponent {
                 return this.masterPasswordScore != null ? this.i18nService.t('weak') : null;
         }
     }
-
-    async submit() {
+    async oidcfill() {
         window.open("https://api.vivokey.com/openid/authorize?response_type=code&scope=openid&client_id=58180502394&state=register&redirect_uri=https://bitwarden.vivokey.com/oidc");
+    }
+    async submit() {
 
         if (this.email == null || this.email === '') {
             this.platformUtilsService.showToast('error', this.i18nService.t('errorOccurred'),
